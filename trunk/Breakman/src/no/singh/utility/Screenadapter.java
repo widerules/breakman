f1255c4883303b85f980514d62e0a5633080abe1
jaspreet.singh.jassal <jaspreet.singh.jassal@0fedb86e-d539-bb40-00c6-ae278b7cfae0>
2012-05-03 22:56:09 +0000

    Har kommet igang. Ballen spretter fint på skjermen og spretter riktig på veggene. Men den tegnes opp på samme canvas om og om igjen. Sliter med å invalidate'e surface view inni thread.

--- trunk/Breakman/src/no/singh/utility/Screenadapter.java
+++ trunk/Breakman/src/no/singh/utility/Screenadapter.java
@@ -1,5 +1,23 @@
 package no.singh.utility;
 
-public class Screenadapter {
+import android.content.Context;
+import android.graphics.Canvas;
 
+/**This class will always save the screen dimensions of the android devices
+ * They will be reachable through their static property
+ *   */
+public class Screenadapter {
+	
+	public static float width;
+	public static float height;
+	public static Context context;
+	
+	
+	public static void inititalizeScreenAdapter(Canvas can, Context con){
+		width = can.getWidth();
+		height = can.getHeight();
+		context = con;
+	}
+	
+	
 }
--- trunk/Breakman/src/no/singh/views/GameView.java
+++ trunk/Breakman/src/no/singh/views/GameView.java
@@ -1,12 +1,109 @@
 package no.singh.views;
 
+import no.singh.models.Ball;
+import no.singh.utility.Screenadapter;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
-public class GameView extends View {
+public class GameView extends SurfaceView implements Runnable, SurfaceHolder.Callback{
+	
+	Thread t = null;
+	SurfaceHolder holder;
+	boolean clearToRun = false;
+	
+	
+	static boolean first = true;
+	static Ball ball; //initiere her??
 
 	public GameView(Context context) {
 		super(context);
+		holder = getHolder();
 	}
+	
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		
+		ball.draw(canvas);
+	}
+	
+	@Override
+	public void run() {
+		first = true;
+		while(clearToRun == true){ 
+			if (!holder.getSurface().isValid()) {
+				continue;
+			}
+			Canvas c = holder.lockCanvas();
+			if(first)initGameView(c, getContext());
+			onDraw(c); 
+			postInvalidate();
+			holder.unlockCanvasAndPost(c); 
+		}
+		
+	} 
+	
+	public void pause(){
+		clearToRun = false;
+		while(true){
+			try{
+				t.join();
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			break;
+		}
+		t = null;
+	}
+	
+	public void resume(){
+		clearToRun = true;
+		t = new Thread(this);
+		t.start();
+	}
+	
+	private static void initGameView(Canvas c, Context cont){
+		Screenadapter.inititalizeScreenAdapter(c, cont);
+		ball = new Ball();
+		first = false;
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		System.out.println(ball.getX());
+		System.out.println(ball.getY());
+		System.out.println((Screenadapter.width/2) - (ball.getImage().getWidth()/2));
+		System.out.println((Screenadapter.height/2) - (ball.getImage().getHeight()/2));
+		return super.onTouchEvent(event);
+	}
+
+
 
 }
