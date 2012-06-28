e0904f356c6207a884d120c455ff72e4428ac456
jaspreet.singh.jassal <jaspreet.singh.jassal@gmail.com>
2012-06-28 19:01:39 +0000

    må få ballen til å sprette

--- Breakman/src/no/singh/utility/Logic.java
+++ Breakman/src/no/singh/utility/Logic.java
@@ -23,12 +23,23 @@ public class Logic {
 //	}
 	
 	public static void detectCollisionHitPoint(Ball b, Chip c){
-		if(c.getNorthEast()[0] - b.getX() < Values.COLLISION_THRESHOLD || c.getNorthWest()[0] - b.getX() < Values.COLLISION_THRESHOLD){
-			b.bounceRight();
-		}
-		else if(c.getSouthWest()[1] - b.getY() < Values.COLLISION_THRESHOLD || c.getNorthEast()[1] - b.getY() < Values.COLLISION_THRESHOLD ){
-			b.bounceDown();
-		}
+		b.stop();
+		System.out.println("Traff chip nr: " +c.getCoord()[0]+", " + c.getCoord()[1] );
+		
+		System.out.println("CHIP x,y: "+ c.getX() + "," + c.getY());
+		System.out.println("CHIP ne "+ c.getNorthEast()[0] + "," + c.getNorthEast()[1]);
+		System.out.println("CHIP nw "+ c.getNorthWest()[0] + "," + c.getNorthWest()[1]);
+		System.out.println("CHIP se "+ c.getSouthEast()[0] + "," + c.getSouthEast()[1]);
+		System.out.println("CHIP sw "+ c.getSouthWest()[0] + "," + c.getSouthWest()[1]);
+		
+		System.out.println("BALL " + b.getX() +","+b.getY());
+		
+//		if(c.getNorthEast()[0] - b.getX() < Values.COLLISION_THRESHOLD || c.getNorthWest()[0] - b.getX() < Values.COLLISION_THRESHOLD){
+//			b.bounceRight();
+//		}
+//		else if(c.getSouthWest()[1] - b.getY() < Values.COLLISION_THRESHOLD || c.getNorthEast()[1] - b.getY() < Values.COLLISION_THRESHOLD ){
+//			b.bounceDown(); 
+//		}
 	}
 	
  
